247f1e33488a7f0ffc737db425075f61ed7e14b5
djbatt <djbatt@live.com>
2018-06-19 18:59:50 -0400

    added comments

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    // user inputs ingredients in their household, out of a list of buttons
+    // ask if sweet or savory // desserts or main meals
+    // depending on sweet or savory, and ingredients listed display recipes
+    // also display meal plan, cooking tips, nutritional facts
+    // use two or three apis to pull this data onto our page
+    // decide on a js library to use
+    // somehow use firebase to store user input data
     var sweet;
     var savory;
 });
