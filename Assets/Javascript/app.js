1ae80552bd7feb08ba1f55f7581be207d5be175b
Prestonmw <prestonwestmoreland@yahoo.com>
2018-06-19 21:40:58 -0400

    added background img and google font

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,48 +1,109 @@
 $(document).ready(function(){
-    // user inputs ingredients in their household, out of a list of buttons
-    // ask if sweet or savory // desserts or main meals
-    // depending on sweet or savory, and ingredients listed display recipes
-    // also display meal plan, cooking tips, nutritional facts
-    // use two or three apis to pull this data onto our page
-    // decide on a js library to use
-    // somehow use firebase to store user input data
 
-    var ingredients = {
-        baking: ["Eggs", "Butter", "Flour", "Honey", "Sugar", "Baking Soda", "Milk"],
-        grains: ["Rice", "Pasta", "Bread"],
-        vegetables: ["Tomato", "Olive", "Beans", "Peanuts", "Lettuce", "Eggplant", "Corn", "Carrot", "Potato"],
-        meats: ["Beef", "Chicken", "Pork"]
-        // cut down ingredients only what we need, when we decide that
+    var ingredients = [{
+            ingredient: "eggs",
+            savory: true,
+            sweet: true
+        }, {
+            ingredient: "butter",
+            savory: true,
+            sweet: true
+        }, {
+            ingredient: "Flour",
+            savory: false,
+            sweet: true
+        }, {
+            ingredient: "Honey",
+            savory: false,
+            sweet: true
+        }, {
+            ingredient: "Sugar",
+            savory: false,
+            sweet: true
+        }, {
+            ingredient: "Baking Soda",
+            savory: false,
+            sweet: true
+        }, {
+            ingredient: "Milk",
+            savory: false,
+            sweet: true
+        }, {
+            ingredient: "Rice",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Pasta",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Bread",
+            savory: true,
+            sweet: true
+        }, {
+            ingredient: "Tomato",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Olive",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Beans",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Peanuts",
+            savory: true,
+            sweet: true
+        }, {
+            ingredient: "Lettuce",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Eggplant",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Corn",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Carrot",
+            savory: true,
+            sweet: true
+        }, {
+            ingredient: "Potato",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Beef",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Peanuts",
+            savory: true,
+            sweet: false
+        }, {
+            ingredient: "Peanuts",
+            savory: true,
+            sweet: false
+        }]
+
+    function flavorCheck() {
+        
     }
+
+    $(document).on("click", ".ingredient", flavorCheck);
 
     function displayIngredients() {
-        for (var i = 0; i < ingredients.baking.length; i++) {
+        for (var i = 0; i < ingredients.length; i++) {
             var ingBtn = $("<button>");
-            ingBtn.addClass("baking");
-            ingBtn.text(ingredients.baking[i]);
+            ingBtn.addClass("ingredient");
+            ingBtn.text(ingredients[i].ingredient);
             $("#ingredientColumn").prepend(ingBtn);
         }
-        for (var i = 0; i < ingredients.grains.length; i++) {
-            var ingBtn = $("<button>");
-            ingBtn.addClass("grains");
-            ingBtn.text(ingredients.grains[i]);
-            $("#ingredientColumn").append(ingBtn);
-        }
-        for (var i = 0; i < ingredients.vegetables.length; i++) {
-            var ingBtn = $("<button>");
-            ingBtn.addClass("vegetables");
-            ingBtn.text(ingredients.vegetables[i]);
-            $("#ingredientColumn").append(ingBtn);
-        }
-        for (var i = 0; i < ingredients.meats.length; i++) {
-            var ingBtn = $("<button>");
-            ingBtn.addClass("meats");
-            ingBtn.text(ingredients.meats[i]);
-            $("#ingredientColumn").append(ingBtn);
-        }
     }
-    var sweet;
-    var savory;
 
     displayIngredients();
 });
