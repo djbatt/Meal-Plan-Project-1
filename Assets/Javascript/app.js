08d083ff5c98be2000d76330fde79766fc4d87e5
djbatt <djbatt@live.com>
2018-06-19 20:47:57 -0400

    on click, run flavor check

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
-    // user inputs ingredients in their household, out of a list of buttons
-    // ask if sweet or savory // desserts or main meals
-    // depending on sweet or savory, and ingredients listed display recipes
-    // also display meal plan, cooking tips, nutritional facts
-    // use two or three apis to pull this data onto our page
-    // decide on a js library to use
-    // somehow use firebase to store user input data
 
     var ingredients = [{
             ingredient: "eggs",
@@ -96,6 +89,12 @@ $(document).ready(function(){
             savory: true,
             sweet: false
         }]
+
+    function flavorCheck() {
+        
+    }
+
+    $(document).on("click", ".ingredient", flavorCheck);
 
     function displayIngredients() {
         for (var i = 0; i < ingredients.length; i++) {
